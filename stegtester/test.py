a72559582978649a6b5ab369ec80a49a51829bee
Chris Ian Connolly <connolly@ai.sri.com>
2014-07-22 11:05:41 -0700

    Missing hamming code in test.py

--- stegtester/test.py
+++ stegtester/test.py
@@ -8,7 +8,58 @@ import sys
 import time
 from subprocess import *
 from random import *
-from hamming import *
+import numpy as N
+import itertools
+
+# Use hamming distance to check messages
+
+fmap = N.zeros(256)
+emap = N.zeros(256)
+
+def byte_hamming(char1, char2):
+    i = 0
+    k1 = ord(char1)
+    k2 = ord(char2)
+
+    x = k1 ^ k2
+
+    for k in range(0,8):
+        if ( x & 1 ): i = i + 1
+        x = x >> 1
+    
+    fmap[k1] += 1
+    if (i > 0): emap[k1] = emap[k1] + 1
+
+    return i
+
+
+#
+# Return the Hamming distance between two strings:
+#
+def hamming(str1, str2):
+    if (len(str1) != len(str2)):
+        print "Different string lengths?"
+        print "Length of string 1 = {:d}".format(len(str1))
+        print "Length of string 2 = {:d}".format(len(str2))
+
+    sum = 0
+    for k in range(0,min(len(str1), len(str2))):
+        sum += byte_hamming(str1[k], str2[k])
+
+    return sum, len(str1)*8
+
+
+def hamming_files(file1, file2):
+    f1 = open(file1, "rb")
+    f2 = open(file2, "rb")
+
+    nbits, total = hamming(f1.read(), f2.read())
+
+    f1.close()
+    f2.close()
+    return nbits, total
+
+
 
 if ( not os.path.isdir('tmp') ):
     os.mkdir('tmp')
