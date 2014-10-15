ea484cca78046899b481e0bc0a1b0cc86175ad8c
Ian A. Mason <iam@csl.sri.com>
2014-10-15 08:03:32 -0700

    tests settle down for now.

--- stegtester/transcode/tests.py
+++ stegtester/transcode/tests.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python
 
 import sys
+
 from PIL import Image
 
 from subprocess import call
 
 import os.path
+
+import random, string
+
+
 
 culprits = [];
 
@@ -13,55 +18,84 @@ failures = 0
 
 successes = 0
 
+
+def randomword(length):
+   return ''.join(random.choice(string.lowercase) for i in range(length))
+
+def generate_message(name, size):
+    file = open(name, "w")
+    file.write(randomword(size))
+    file.close()
+    
+
 def transcode(filein, fileout, target_jpeg_quality):
     img = Image.open(filein)
     img.save(fileout, "JPEG", quality=target_jpeg_quality)
     
 
-def wedge_unwedge(dir, imageno):
-    global successes
-    global failures
+def different(filein, fileout):
+    sizein  = os.stat(filein).st_size
+    sizeout = os.stat(fileout).st_size
+    retval = True
+    if sizein == sizeout:
+        fin = open(filein, 'r')
+        sin = fin.read(sizein)
+        fout = open(fileout, 'r')
+        sout = fout.read(sizein)
+        index = 0
+        for i , o in zip(sin, sout):
+            if not i == o:
+                print 'different at index ', index, ' in = ', i, ' out = ', o
+                return retval
+            index += 1
+        retval = False
+    os.remove(fileout)
+    return retval
+
+
+def wedge_unwedge(dir, imageno, messagein, messageout):
+    global successes, failures
     image = str(imageno)
-    call(['wedge', '-message', '0123456789', dir + '/' + image + '.jpg',  image + '_jel.jpg'])
-    call(['unwedge', image + '_jel.jpg', 'text.txt'])
-    failure = call(['diff', 'text.txt', 'orig.bin'])
+    message_length  = str(os.stat(messagein).st_size)
+    call(['wedge', '-nolength', '-data', messagein, dir + '/' + image + '.jpg',  image + '_jel.jpg'])
+    call(['unwedge', '-length', message_length, image + '_jel.jpg', messageout])
+    failure = different(messagein, messageout)
     if failure:
         print "wedge-unwedge %s FAILED\n" % format( image + '.jpg')
-        failure += 1
-    else:
-        print "wedge-unwedge %s OK\n" % format( image + '.jpg')
-        successes += 1
+        exit()
+    #else:
+    #    print "wedge-unwedge %s OK\n" % format( image + '.jpg')
 
-def wedge_transcode_unwedge(dir, imageno):
-    global successes
-    global failures
-    global culprits
+
+def wedge_transcode_unwedge(dir, imageno, messagein, messageout):
+    global successes, failures, culprits
     image = str(imageno)
     source = dir + '/' + image + '.jpg'
-    call(['wedge', '-message', '0123456789', source,  image + '_jel.jpg'])
+    message_length  = str(os.stat(messagein).st_size)
+    call(['wedge', '-nolength', '-data', messagein, dir + '/' + image + '.jpg',  image + '_jel.jpg'])
     transcode(image + '_jel.jpg',  image + '_jel_tr.jpg', 30)
-    call(['unwedge', image + '_jel_tr.jpg', 'text.txt'])
-    failure = call(['diff', 'text.txt', 'orig.bin'])
+    call(['unwedge', '-length', message_length, image + '_jel_tr.jpg', messageout])
+    failure = different(messagein, messageout)
     if failure:
         print "wedge_transcode_unwedge %s FAILED\n" % format( image + '.jpg')
         failures += 1
         culprits.append(source);
-
-
     else:
         print "wedge_transcode_unwedge %s OK\n" % format( image + '.jpg')
         successes += 1
 
+
+generate_message("message_in.bin", 10)
+
 for imageno in range(101, 500):
     image = str(imageno)
     if os.path.exists( 'q30/' + image + '.jpg' ):
-        wedge_unwedge('q30', imageno)
-        wedge_transcode_unwedge('q30', imageno)
+        wedge_unwedge('q30', imageno, "message_in.bin", "message_out.bin")
+        wedge_transcode_unwedge('q30', imageno, "message_in.bin", "message_out.bin")
 
 print "Successes: ", successes
 
 print "Failures: ", failures
-
 
 print "Culprits: ", culprits
 
