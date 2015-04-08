b7a3c371f480f9222d3331c64b0e592ef34cd026
Ian A. Mason <iam@csl.sri.com>
2015-04-08 09:41:03 -0700

    Travis tweaks.

--- .travis/build.sh
+++ .travis/build.sh
@@ -2,14 +2,19 @@
 # Make sure we exit if there is a failure
 set -e
 
-#!/bin/bash -x
-# Make sure we exit if there is a failure
-set -e
-
-export CC=gcc
-
 autoreconf -i
 ./configure --enable-silent-rules
 
 make clean
 make
+RETURN="$?"
+
+
+if [ "${RETURN}" != "0" ]; then
+    echo "Building jel failed!"
+    exit 1
+fi
+
+
+
+
