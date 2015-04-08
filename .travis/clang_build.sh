821cd7b329c7e18c0d0a7f9c32101aff8ef728e2
Ian A. Mason <iam@csl.sri.com>
2015-04-08 09:28:52 -0700

    More travising...

--- .travis/clang_build.sh
+++ .travis/clang_build.sh
@@ -1,3 +1,13 @@
 #!/bin/bash -x
 # Make sure we exit if there is a failure
 set -e
+
+export CC=clang
+
+autoreconf -i
+./configure --enable-silent-rules
+
+make clean
+make
+
+
--- .travis/gcc_build.sh
+++ .travis/gcc_build.sh
@@ -2,3 +2,14 @@
 # Make sure we exit if there is a failure
 set -e
 
+#!/bin/bash -x
+# Make sure we exit if there is a failure
+set -e
+
+export CC=gcc
+
+autoreconf -i
+./configure --enable-silent-rules
+
+make clean
+make
